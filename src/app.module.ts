28cc8243bfdb4f29ef3b348c354b525dc88c9b35
pragya2611 <pragyat2611@gmail.com>
2023-08-17 12:56:21 +0530

    mongo-connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [
+        ConfigModule.forRoot({
+          load: [configuration],
+        }),
+      ],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('database.uri'), // Loaded from .ENV
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
